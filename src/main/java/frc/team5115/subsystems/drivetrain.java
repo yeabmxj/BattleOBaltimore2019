540a75d2206cd8d807076302b408161f34a1a2d1
Forrest <fmilner@zoom456.com>
2019-09-11 16:12:36 -0400

    Started drivetrain

--- src/main/java/frc/team5115/subsystems/drivetrain.java
+++ src/main/java/frc/team5115/subsystems/drivetrain.java
@@ -1,7 +1,27 @@
 package frc.team5115.subsystems;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import frc.team5115.robot.Robot;
 
-import frc.team5115.Robot;
+
 
 public class drivetrain {
-	
+
+    TalonSRX frontLeft;
+    TalonSRX frontRight;
+    TalonSRX backLeft;
+    TalonSRX backRight;
+
+    public drivetrain() {
+        frontLeft = new TalonSRX(0);
+        frontRight = new TalonSRX(1);
+        backLeft = new TalonSRX(2);
+        backRight = new TalonSRX(3);
+
+        frontLeft.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+        frontRight.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+        backLeft.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+        backRight.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+    }
+    
 }
