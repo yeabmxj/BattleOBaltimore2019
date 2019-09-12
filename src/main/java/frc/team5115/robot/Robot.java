2df70f017816115beeb78a87f8f6e264bb8bb6e2
Forrest <fmilner@zoom456.com>
2019-09-12 15:15:30 -0400

    V1 Complete

--- src/main/java/frc/team5115/robot/Robot.java
+++ src/main/java/frc/team5115/robot/Robot.java
@@ -1,10 +1,14 @@
 package frc.team5115.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import frc.team5115.subsystems.drivetrain;
 
 public class Robot extends TimedRobot {
-//Olivia
-//Yeab
 
+    public static drivetrain dt;
+
+    public void robotInit() {
+        dt = new drivetrain();
+    }
 }
 
--- src/main/java/frc/team5115/subsystems/drivetrain.java
+++ src/main/java/frc/team5115/subsystems/drivetrain.java
@@ -1,9 +1,8 @@
 package frc.team5115.subsystems;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import frc.team5115.robot.Robot;
-
-
 
 public class drivetrain {
 
@@ -23,5 +22,22 @@ public class drivetrain {
         backLeft.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
         backRight.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
     }
-    
+
+    public void drive(double x, double y, double thrott) {
+        double leftSpd = (y + x) * thrott;
+        double rightSpd= (y - x) * thrott;
+
+        frontLeft.set(ControlMode.PercentOutput, leftSpd);
+        backLeft.set(ControlMode.PercentOutput, leftSpd);
+        frontRight.set(ControlMode.PercentOutput, rightSpd);
+        backRight.set(ControlMode.PercentOutput, rightSpd);
+
+    }
+
+    public void resetEncoders() {
+        frontLeft.set(ControlMode.PercentOutput, 0);
+        backLeft.set(ControlMode.PercentOutput, 0);
+        frontRight.set(ControlMode.PercentOutput, 0);
+        backRight.set(ControlMode.PercentOutput, 0);
+    }
 }
