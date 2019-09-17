75d13d0280a0772f8c027650db9bdd2e1078d133
marieseay <sheltmarie11@gmail.com>
2019-09-17 16:01:52 -0400

    intake

--- src/main/java/frc/team5115/robot/Robot.java
+++ src/main/java/frc/team5115/robot/Robot.java
@@ -1,10 +1,25 @@
 package frc.team5115.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import frc.team5115.subsystems.intakeMech;
 
 public class Robot extends TimedRobot {
-//Olivia
-//Yeab
-//marie
+
+    public static intakeMech intakemech;
+    public static Joystick joystick;
+
+    public void robotInit()
+    {
+        intakemech = new intakeMech();
+        joystick = new Joystick(0);
+
+        intakemech.intakeStill();
+    }
+
+    public void teleopPeriodic()
+    {
+        intakemech.intakeOpen();
+    }
 }
 
